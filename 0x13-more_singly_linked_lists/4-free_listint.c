00b442239d909ea97a5649ded114494dc2ee921b
honeybadger05 <aboulkacemzakaria5@gmail.com>
2023-11-21 12:10:50 -0800

    fixes

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,17 +3,16 @@
  * free_listint - frees the list
  * @head: head of struct
  *
+ * Return: void
  */
 void free_listint(listint_t *head)
 {
 	listint_t *tmp;
 
-
-	while (head != NULL)
+	while (head)
 	{
 		tmp = head;
-		free(head);
 		head = tmp->next;
+		free(tmp);
 	}
-
 }
