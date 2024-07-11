b0ff45ac8cc6b522fbdfa61fecc6f27773ab949b
honeybadger05 <144347919+honeybadger05@users.noreply.github.com>
2024-07-11 10:07:55 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,39 +1,28 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
-* _sqrt_helper - fucntion name
-* @n: number
-* @i: index
-*
-* Description: a function that calculates natural sqrt
-* Return: result
-*/
-
-int _sqrt_helper(int n, int i)
+  * square_root -  a function that returns a root square
+  * @n: integer
+  * @y: integer
+  * Return: 0
+  */
+int square_root(int n, int y)
 {
-	int sqrt = i * i;
-
-	if (sqrt > n)
-	{
+	if (n * n == y)
+		return (n);
+	else if (n * n > y)
 		return (-1);
-	}
-	if (sqrt == n)
-	{
-		return (i);
-	}
-
-	return (_sqrt_helper(n, i + 1));
+	else
+		return (square_root(n + 1, y));
 }
-
 /**
-* _sqrt_recursion - function name
-* @n: number..
-*
-* Description: a function that recursively returns nsqrt
-* Return: result
-*/
-
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ * Return: the natural square root found
+ */
 int _sqrt_recursion(int n)
 {
-	return  (_sqrt_helper(n, 1));
+	int a = 1;
+
+	return (square_root(a, n));
 }
