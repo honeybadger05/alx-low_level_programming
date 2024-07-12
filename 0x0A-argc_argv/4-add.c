9f276f449db75e5da525fe446ee8b4ee4230596e
honeybadger05 <144347919+honeybadger05@users.noreply.github.com>
2024-07-12 23:14:08 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,74 +1,58 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <ctype.h>
 /**
-  * _isdigit - tests the string is an representation of an iteger
-  * @s: the string
-  *
-  * Return: 0 if it is not an int or 1 if it is
-  */
-int _isdigit(char *s)
+ * StringCheck - checks string
+ * @s: string to check
+ * Return: something
+ */
+int StringCheck(char *s)
 {
-	if (*s && *s == '-')
-		s++;
-	while (*s)
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
 	{
-		if (!(*s >= '0' && *s <= '9'))
+		if (!isdigit(s[i]))
+		{
 			return (0);
-		s++;
+		}
 	}
 	return (1);
 }
-
 /**
-  * atoi - convert string of ints to integers
-  * @s: the string representation of the integer
-  *
-  * Return: int
-  */
-int atoi(char *s)
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+int main(int argc, char  *argv[])
 {
-	int num = 0;
-	int sign = 1;
+	int i;
+	int result = 0;
 
-	if (*s && *s == '-')
+	if (argc > 1)
 	{
-		sign = -1;
-		s++;
-	}
-	while (*s && *s >= '0' && *s <= '9')
-	{
-		num = num * 10 + (*s - '0');
-		s++;
-	}
-	return (num * sign);
-}
-
-/**
-  * main - the entry point of our program
-  * @argc: the number of arguments
-  * @argv: the 2d pointer of arguments
-  *
-  * Return: always 0
-  */
-int main(int argc, char **argv)
-{
-	int sum = 0;
-	int i = 1;
-
-	if (argc < 2)
-	{
-		printf("%d\n", sum);
+		for (i = 1; i < argc; i++)
+		{
+			if (StringCheck(argv[i]))
+			{
+				result += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", result);
 		return (0);
 	}
-	while (i < argc)
+	else
 	{
-		if (!_isdigit(argv[i]))
-		{
-			printf("%s\n", "Error");
-			return (1);
-		}
-		sum += atoi(argv[i++]);
+		printf("%d\n", 0);
+		return (1);
 	}
-	printf("%d\n", sum);
-	return (0);
+
+}
+
